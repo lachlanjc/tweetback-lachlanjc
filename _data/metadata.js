c013369a8b9fccd5ec992eb33e3a30fd166603e2
Lachlan Campbell <lachlanjc@hey.com>
2023-01-16 03:29:45 -0800

    Generate site

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,9 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "lachlanjc", // No leading @ here
+	homeLabel: "lachlanjc.com",
+	homeUrl: "https://lachlanjc.com/",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `https://github.com/lachlanjc.png`;
 
 module.exports = data;
